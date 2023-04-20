ae74610f79b7494d1d315286c64cc777d44eef88
slivcaigor <slivcaigor@gmail.com>
2023-04-20 15:45:31 +0200

    Add project files.

---
+++ adonet-db-videogame/VideogameManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adonet_db_videogame
+{
+    internal class VideogameManager
+    {
+    }
+}
